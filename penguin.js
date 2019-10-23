1820ad81beaab3b0cc8c6229298661f3f7baf56f
Croslin D. Johnson <croslin.johnson@centre.edu>
2019-10-23 16:14:50 -0400

    Added new sections to the table

--- penguin.js
+++ penguin.js
@@ -1,20 +1,44 @@
 var penPromise=d3.json("penguins/penguins/classData.json")
 penPromise.then(function(penData)
                 {
-    GetImages(penData)
     console.log(GetQuizMean(penData[0].quizes))
+    console.log(GetHomeworkMean(penData[0].homework))
+    console.log(GetTestMean(penData[0].test))
+    GetImages(penData)
     console.log("penData", penData);
 },
                 function(err)
 {
     console.log("fail",err)
 })
+
 var GetQuizMean= function(quizes)
 {
    var quizGrades = quizes.map(function (quiz) {
                           return quiz.grade
                           })
     return d3.mean(quizGrades)
+}
+
+var GetHomeworkMean= function(homework)
+{
+    var hwGrades = homework.map(function (hw){
+        return hw.grade
+    })
+    return d3.mean(hwGrades)
+}
+
+var GetTestMean = function(tests)
+{
+    var testGrades = tests.map(function (test){
+        return test.grade
+    })
+    return d3.mean(testGrades)
+}
+
+var GetGrade = function(assignments)
+{
+    
 }
     
 var GetImages=function(penData){
@@ -32,9 +56,27 @@ var row= d3.select("tbody").append("tr")
     row.append("td")
         .text(function(d)
         {
-        return GetQuizMean(penData.quizes);
+        return GetQuizMean(d.quizes);
         })
     row.append("td")
+        .text(function(d)
+        {
+        return GetHomeworkMean(d.homework);
+        })
     row.append("td")
-
+        .text(function(d)
+        {
+        return GetTestMean(d.test);
+        })
+    row.append("td")
+        .text(function(d)
+        {
+        return d.final;
+        })
+    row.append("td")
+        .text(function(d)
+        {
+        return GetGrade
+        })
+    
 }
