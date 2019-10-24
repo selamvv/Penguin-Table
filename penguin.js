d24d2cdf7978df6c80beeec77c0122ac4a7b59be
selamvv <selam.vanvoorhis@centre.edu>
2019-10-24 01:43:44 -0400

    m

--- penguin.js
+++ penguin.js
@@ -1,8 +1,8 @@
+
 var penPromise=d3.json("penguins/penguins/classData.json")
 penPromise.then(function(penData)
                 {
     GetImages(penData)
-    console.log(GetQuizMean(penData[0].quizes))
     console.log("penData", penData);
 },
                 function(err)
@@ -11,30 +11,78 @@ penPromise.then(function(penData)
 })
 var GetQuizMean= function(quizes)
 {
-   var quizGrades = quizes.map(function (quiz) {
+   var quizGrades = quizes.map(function(quiz) {
                           return quiz.grade
                           })
     return d3.mean(quizGrades)
-}
     
+}
+var GetHwMean=function(homework)
+{
+    var hwGrades= homework.map(function(hw){
+        return hw.grade
+    })
+    
+    return d3.mean(hwGrades)
+}
+var GetTestMean= function(test){
+    
+    var testGrades= test.map(function(test){
+        return test.grade
+    })
+    return d3.mean(testGrades)
+}
+var Getfinal= function(final){
+    finalScore= final.map(function(d){
+        return d.grade})
+    return d3.mean(finalScore)}
+
+var GetScore= function(grade){
+    var score =
+        GetQuizMean(grade)*.2 +
+                GetHwMean(grade)*
+                .15+GetTestMean(grade)*
+                .3+Getfinal(grade)*.35
+    return score;}
+
+
+
 var GetImages=function(penData){
-var row= d3.select("tbody").append("tr")
+ 
+var row= d3.select("tbody")
     .selectAll("tr")
     .data(penData)
     .enter()
     .append("tr")
-    row.append("img")
+    row.append("td").append("img")
         .attr("src", function(d)
-         {
+        {
         return "penguins/penguins/" + d.picture;
-    })
-    
+        })
     row.append("td")
         .text(function(d)
         {
-        return GetQuizMean(penData.quizes);
+        return GetQuizMean(d.quizes);
         })
     row.append("td")
+        .text(function(d)
+        {
+        return GetHwMean(d.homework);      
+        })
     row.append("td")
-
+        .text(function(d)
+             {
+        return GetTestMean(d.test);
+        })
+    row.append("td")
+        .text(function(d)  
+              {   
+        return Getfinal(d.final);})
+      //style("color", 
+       // .text (function(d){
+        //    var troublegrade= Getfinal(d)
+        //    if (troublegrade<70){
+         //       text.style.color= "orange"}})
 }
+          
+    
